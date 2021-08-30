2b33bd7dacba59c3ba72f883965d01d681b012b8
ianj090 <ianjenatz@ufm.edu>
2021-08-30 09:52:42 -0600

    Added user edit profile functionality and profile info display

--- profiles.py
+++ profiles.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, g, request, flash, render_template
+from flask import Flask, redirect, g, request, flash, render_template, session
 from peewee import * # ORM for Sqlite
 from hashlib import md5 # Encoder for password
 
@@ -23,11 +23,28 @@ class User(BaseModel):
     username = CharField(unique=True)
     password = CharField()
     email = CharField()
+    firstName = CharField()
+    lastName = CharField()
+    country = CharField()
+    birthday = CharField()
+    occupation = CharField()
+    mobile_number = CharField()
+    phone_number = CharField()
 
 # Creates table in database
 def create_table():
     with database:
         database.create_tables([User])
+
+# Set Flask session variable
+def auth_user(user):
+    session['logged_in'] = True
+    session['username'] = user.username
+
+# get the user from the session
+def get_current_user():
+    if session.get('logged_in'):
+        return User.get(User.username == session['username'])
 
 # Open and close database connection with every request
 @app.before_request
@@ -55,6 +72,8 @@ def login():
             user = User.get(
                 (User.username == request.form['inputUsername']) &
                 (User.password == pw_hash))
+            auth_user(user)
+            
             return redirect('/profile')
 
         # If user does not exist then it creates the new user.
@@ -64,8 +83,18 @@ def login():
                     user = User.create(
                         username = request.form['inputUsername'],
                         password = md5((request.form['inputPassword']).encode('utf-8')).hexdigest(),
-                        email = ""
+                        email = "",
+                        firstName = "",
+                        lastName = "",
+                        country = "",
+                        birthday = "",
+                        occupation = "",
+                        mobile_number = "",
+                        phone_number = ""
                     )
+                
+                auth_user(user)
+
                 return redirect('/profile')
 
             # If user exists and password does not match, reload the login page.
@@ -75,14 +104,45 @@ def login():
         
     return render_template('login.html')
 
-@app.route('/profile')
+@app.route('/profile', methods=['GET', 'POST'])
 def homepage():
-    
-    return render_template('profile.html')
+    # if method is post, search for user in DB using form, if method is get use flask session variable
+    if request.method == "POST":
+        user = User.get(User.username == request.form['searchUsername'])
+        # return render_template('profile.html')
+    else:
+        user = get_current_user()
+
+    return render_template('profile.html',
+        username = user.username,
+        email = user.email,
+        firstName = user.firstName,
+        lastName = user.lastName,
+        country = user.country,
+        birthday = user.birthday,
+        occupation = user.occupation,
+        mobile_number = user.mobile_number,
+        phone_number = user.phone_number
+    )
 
 # Create / Edit Profile information
-@app.route('/edit')
+@app.route('/edit', methods=['GET', 'POST'])
 def edit():
+    if request.method == "POST":
+        current_user = get_current_user()
+        query = User.update(
+            email = request.form['editEmail'],
+            firstName = request.form['editFirstName'],
+            lastName = request.form['editLastName'],
+            country = request.form['editCountry'],
+            birthday = request.form['editBirthday'],
+            occupation = request.form['editOccupation'],
+            mobile_number = request.form['editMobileNumber'],
+            phone_number = request.form['editPhoneNumber']
+        ).where(User.username == current_user.username).execute()
+
+        return redirect('/profile')
+
     return render_template('editProfile.html')
 
 
